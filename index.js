b56a96c7bce22f244d5f742be1adc38067fbf827
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-28 17:26:25 -0300

    cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 const situationsRoutesV1 = require('./routes/v1/situations-routes');
 
