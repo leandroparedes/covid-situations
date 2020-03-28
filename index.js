97c116ff441f25e301cb52a4a9c6a36684b5ca38
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-28 17:37:01 -0300

    test format and endpoint for testing

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ app.use(cors());
 
 const situationsRoutesV1 = require('./routes/v1/situations-routes');
 
+// for testing purposes
+app.get('/', (req, res) => {
+    const data = require('./format_test.json');
+
+    /* data.forEach(year => {
+        console.log('Year:', year.year);
+        year.months.forEach(month => {
+            console.log('Month:', month.month);
+            month.days.forEach(day => {
+                console.log('Day:', day.day);
+                day.situation.forEach(situation => {
+                    console.log('Situation report:', situation);
+                });
+
+                if (day.hasOwnProperty('preparedness_and_responses')) {
+                    day.preparedness_and_responses.forEach(action => {
+                        console.log('Authority:', action.authority);
+
+                        action.responses.forEach(response => {
+                            console.log('Response:', response);
+                        });
+                    });
+                }
+            });
+        });
+        console.log('---');
+    }); */
+    
+    res.send(data);
+});
+
 app.use('/v1/situations', situationsRoutesV1);
 
 const host = '0.0.0.0';
