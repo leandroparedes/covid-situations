8395bc9fba17fb7acd73aed910954e3c4d0d98ad
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-28 19:20:55 -0300

    disable routes

--- routes/v1/situations-routes.js
+++ routes/v1/situations-routes.js
@@ -4,36 +4,15 @@ const router = express.Router();
 const data = require('./situations.json');
 
 router.get('/:year', (req, res) => {
-    const yearData = data[req.params.year];
-
-    if (! yearData) {
-        res.send({ message: 'No hay datos para el año ' + req.params.year });
-        return;
-    }
-
-    res.send(yearData);
+    res.send('Feature not available at the moment');
 });
 
 router.get('/:year/:month', (req, res) => {
-    const monthData = data[req.params.year][req.params.month];
-
-    if (! monthData) {
-        res.send({ message: `No hay datos para el mes ${req.params.month} del año ${req.params.year}` });
-        return;
-    }
-
-    res.send(monthData);
+    res.send('Feature not available at the moment');
 });
 
 router.get('/:year/:month/:day', (req, res) => {
-    const dayData = data[req.params.year][req.params.month]['days'][req.params.day];
-
-    if (! dayData) {
-        res.send({ message: `No hay datos para el dia ${req.params.day} del mes ${req.params.month} del año ${req.params.year}` });
-        return;
-    }
-
-    res.send(dayData);
+    res.send('Feature not available at the moment');
 });
 
 router.get('/', (req, res) => {
