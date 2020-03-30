b2b8692d3e66d9c6b10b6a8550e2f35e1d14fdde
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-29 21:51:45 -0300

    paginate

--- routes/v1/situations-routes.js
+++ routes/v1/situations-routes.js
@@ -3,6 +3,33 @@ const router = express.Router();
 
 const data = require('./situations.json');
 
+const apiUrl = `https://covid-situations.herokuapp.com/v1`;
+
+function paginate (pageNumber, perPage) {
+    const itemsPerPage = perPage || 10;
+
+    let end = pageNumber * itemsPerPage;
+    let start = end - itemsPerPage;
+
+    let paginatedData = {};
+    paginatedData.data = data.slice(start, end);
+
+    let links = {};
+    if (start > 0) {
+        let page = parseInt(pageNumber) - 1;
+        links.prev = `${apiUrl}/situations?page=${page}&perPage=${itemsPerPage}`;
+    }
+
+    if (end < data.length) {
+        let page = parseInt(pageNumber) + 1;
+        links.next = `${apiUrl}/situations?page=${page}&perPage=${itemsPerPage}`;
+    }
+
+    paginatedData.links = links;
+
+    return paginatedData;
+}
+
 /**
  * Return the last 10 situations reports
  */
@@ -16,6 +43,14 @@ router.get('/latest', (req, res) => {
  * Return all situations reports
  */
 router.get('/', (req, res) => {
+
+    // send paginated response
+    if (req.query.page) {
+        const paginatedData = paginate(req.query.page, req.query.perPage);
+        res.send(paginatedData);
+        return;
+    }
+
     res.send(data);
 });
 
